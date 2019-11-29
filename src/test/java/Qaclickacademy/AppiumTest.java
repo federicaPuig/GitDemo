80de9761bff1a8863856b8b43bb85b66aa9b421d
fede <federica.puig.r@gmail.com>
2019-11-29 16:37:14 -0600

    first commit

---
+++ src/test/java/Qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package Qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeAPpAndroid() {
+		
+		System.out.println("nativeAPpAndroid");
+	}
+	
+	@Test
+	public void IOSApp () {
+		System.out.println("IOSApp");
+	}
+
+}
---
+++ src/test/java/Qaclickacademy/RESTAPITest.java
@@ -0,0 +1,17 @@
+package Qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwiter() {
+		System.out.println("deleteTwiter");
+	}
+
+}
